671f1cd7951c8b48b7c787b6ab92d5b1d6d4b4d8
HungYuHei <kongruxi@gmail.com>
2012-04-20 23:17:38 +0800

    store oauth token and secret

--- app/models/user/omniauth_callbacks.rb
+++ app/models/user/omniauth_callbacks.rb
@@ -5,6 +5,7 @@ class User
       define_method "find_or_create_for_#{provider}" do |response|
         uid = response["uid"]
         data = response["info"]
+        credentials = response['credentials']
 
         if user = User.where("authorizations.provider" => provider , "authorizations.uid" => uid).first
           user
@@ -15,7 +16,9 @@ class User
           user = User.new_from_provider_data(provider, uid, data)
 
           if user.save(:validate => false)
-            user.authorizations << Authorization.new(:provider => provider, :uid => uid )
+            user.authorizations << Authorization.new(:provider => provider, :uid => uid,
+                                                     :token => credentials['token'],
+                                                     :secret => credentials['secret'])
             return user
           else
             Rails.logger.warn("User.create_from_hash 失败，#{user.errors.inspect}")
