ada7105913e217f28fe4b9a1f6449b15c67a2ff7
HungYuHei <kongruxi@gmail.com>
2012-05-23 13:59:13 +0800

    specify redis db option

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,8 +4,8 @@ require "redis/objects"
 
 redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 
-Redis::Objects.redis = Redis.new(:host => redis_config['host'], :port => redis_config['port'])
+Redis::Objects.redis = Redis.new(:host => redis_config['host'], :port => redis_config['port'], :db => redis_config['db'])
 
 Resque::Mailer.default_queue_name = "mailer"
-Resque.redis = Redis.new(:host => redis_config['host'],:port => redis_config['port'])
-Resque.redis.namespace = "resque:ruby-china"
+Resque.redis = Redis.new(:host => redis_config['host'], :port => redis_config['port'], :db => redis_config['db'])
+Resque.redis.namespace = "resque:mg_forum"
