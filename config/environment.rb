2af2786a4cf913739036656f350a20c0a2a8adfc
HungYuHei <kongruxi@gmail.com>
2012-05-23 14:20:54 +0800

    re-establish Redis connection when a new Passenger worker is created

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,18 @@ require File.expand_path('../application', __FILE__)
 RubyChina::Application.initialize!
 
 SITE_NAME = Setting.app_name
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # We're in smart spawning mode.
+    if forked
+      # Re-establish redis connection
+      require 'redis'
+      redis_config = YAML.load_file("#{Rails.root.to_s}/config/redis.yml")[Rails.env]
+
+      # The important two lines
+      $redis.client.disconnect
+      $redis = Redis.new(:host => redis_config['host'], :port => redis_config['port'], :db => redis_config['db'])
+    end
+  end
+end
